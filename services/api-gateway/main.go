255207af095ec01394ef2fa8d07841f69d6a02ca
huynguyen1310 <qhuy131096@gmail.com>
2026-07-16 18:48:27 +0700

    graceful shutdown

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"ride-sharing/shared/env"
 )
@@ -23,7 +28,28 @@ func main() {
 		Handler: mux,
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatal("Failed to start server: ", err)
+	serverError := make(chan error, 1)
+
+	go func() {
+		log.Printf("server listening on %s", httpAddr)
+		serverError <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case sig := <-shutdown:
+		log.Printf("Server is shutting down due to %v signal", sig)
+	case err := <-serverError:
+		log.Printf("Error starting the server: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Could not shutdown the server: %v", err)
+		server.Close()
 	}
 }
--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -2,19 +2,56 @@ package main
 
 import (
 	"context"
-	"ride-sharing/services/trip-service/internal/domain"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	h "ride-sharing/services/trip-service/internal/infrastructure/http"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
 )
 
 func main() {
-	ctx := context.Background()
+	log.Println("Starting Trip Service")
+
 	inmemRepo := repository.NewInmemRepository()
 	svc := service.NewService(inmemRepo)
-	fare := &domain.RideFareModel{
-		UserID: "13",
+	mux := http.NewServeMux()
+
+	httpHandler := h.HttpHandler{Service: svc}
+
+	mux.HandleFunc("POST /preview", httpHandler.HandleTripPreview)
+
+	server := &http.Server{
+		Addr:    ":8083",
+		Handler: mux,
 	}
 
-	svc.CreateTrip(ctx, fare)
+	serverError := make(chan error, 1)
 
+	go func() {
+		log.Printf("server listening on %s", server.Addr)
+		serverError <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case sig := <-shutdown:
+		log.Printf("Server is shutting down due to %v signal", sig)
+	case err := <-serverError:
+		log.Printf("Error starting the server: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Could not shutdown the server: %v", err)
+		server.Close()
+	}
 }
